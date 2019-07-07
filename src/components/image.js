19133591dc97af58ec445c16245181f69b9af6e1
lilairpod <pornreadsapp@gmail.com>
2019-07-07 11:19:52 -0400

    delete unused images

--- src/components/image.js
+++ src/components/image.js
@@ -4,16 +4,16 @@ import { useStaticQuery, graphql } from "gatsby"
 
 const Image = ({ query }) => {
   const data = useStaticQuery(graphql`
-  query {
-    placeholderImage: file(relativePath: { eq: "max-color.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
+    query {
+      placeholderImage: file(relativePath: { eq: "max-dog.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
-  }
-`)
+  `)
 
   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
